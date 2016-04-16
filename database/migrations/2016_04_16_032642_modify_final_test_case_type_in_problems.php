34a02dfbb9b4aaedc4a9c843a0ea72edbd812935
SSSStar <619666889@qq.com>
2016-04-16 14:19:09 +0800

    problems table modifications

---
+++ database/migrations/2016_04_16_032642_modify_final_test_case_type_in_problems.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyFinalTestCaseTypeInProblems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('problems', function (Blueprint $table) {
+            $table->dropColumn('final_test_case_address');//删除最终测试用例路径
+            $table->string('final_test_case_address_in');//最终测试输入用例路径
+            $table->string('final_test_case_address_out');//最终测试输出用例路径
+            $table->string('random_key');//随机生成的特征码
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('problems', function (Blueprint $table) {
+            $table->string('final_test_case_address');//删除最终测试用例路径
+            $table->dropColumn('final_test_case_address_in');//最终测试输入用例路径
+            $table->dropColumn('final_test_case_address_out');//最终测试输出用例路径
+            $table->dropColumn('random_key');//随机生成的特征码
+        });
+    }
+}
